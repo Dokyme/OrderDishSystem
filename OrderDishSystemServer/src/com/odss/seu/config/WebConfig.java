fe69eb74f63521438261d6a4753185c5e4198d2a
Polydick <zdk_cz@sina.comom>
2017-09-04 16:35:48 +0800

    second commit

---
+++ OrderDishSystemServer/src/com/odss/seu/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.odss.seu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc //启用springmvc
+@ComponentScan("com.odss.seu") //自动扫描装配
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public ViewResolver viewResolver(){
+        //启用视图解析器
+        InternalResourceViewResolver resourceViewResolver=new InternalResourceViewResolver();
+        resourceViewResolver.setPrefix("/WEB-INF/views/");
+        resourceViewResolver.setSuffix(".jsp");
+        resourceViewResolver.setExposeContextBeansAsAttributes(true);
+        return resourceViewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        //配置静态资源的处理
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver cnViewResolver(ContentNegotiationManager cnm){
+        ContentNegotiatingViewResolver cnvr=new ContentNegotiatingViewResolver();
+        cnvr.setContentNegotiationManager(cnm);
+        return cnvr;
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
