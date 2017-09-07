d1ff25704a32975ebc22851b4ecd9327ccb1477b
TaoJiangChuan <1076056766@qq.com>
2017-09-07 11:44:32 +0800

    ffff

--- OrderDishSystemServer/src/main/java/com/odss/seu/service/exception/UsernameNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.odss.seu.service.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.GONE, reason = "用户名不存在")
-public class UsernameNotFoundException extends RuntimeException {
-
-}
