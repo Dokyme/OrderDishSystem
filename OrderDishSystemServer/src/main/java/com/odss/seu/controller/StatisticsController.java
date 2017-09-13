4b57295ba06a8f43c9b3fc723a7d21e3dc35dcdc
Polydick <zdk_cz@sina.com>
2017-09-13 09:49:54 +0800

    commit 0913

--- OrderDishSystemServer/src/main/java/com/odss/seu/controller/StatisticsController.java
+++ OrderDishSystemServer/src/main/java/com/odss/seu/controller/StatisticsController.java
@@ -10,31 +10,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/statistics")
 public class StatisticsController {
-//    起始时间，结束时间
-//    按照年，按照月，按照天
-
     //    按照年
     @RequestMapping(value = "/byyear", method = RequestMethod.GET)
     public Statistics queryStatisticsByYear(Integer startTime, Integer endTime) {
-        Statistics statistics = StatisticsQueryService.queryAllStatisticsByYear(startTime, endTime);
-        return statistics;
+//        Statistics statistics = StatisticsQueryService.queryAllStatisticsByYear(startTime, endTime);
+//        return statistics;
     }
 
 
     //    按照月
     @RequestMapping(value = "/bymonth", method = RequestMethod.GET)
     public Statistics queryStatisticsByMonth(Integer startTime, Integer endTime) {
-        Statistics statistics = StatisticsQueryService.queryAllStatisticsByMonth(startTime, endTime);
-        return statistics;
+//        Statistics statistics = StatisticsQueryService.queryAllStatisticsByMonth(startTime, endTime);
+//        return statistics;
     }
 
     //    按照天
     @RequestMapping(value = "/byday", method = RequestMethod.GET)
     public Statistics queryStatisticsByDay(Integer startTime, Integer endTime) {
-        Statistics statistics = StatisticsQueryService.queryAllStatisticsByDay(startTime, endTime);
-        return statistics;
+//        Statistics statistics = StatisticsQueryService.queryAllStatisticsByDay(startTime, endTime);
+//        return statistics;
     }
-
 }
 
 
