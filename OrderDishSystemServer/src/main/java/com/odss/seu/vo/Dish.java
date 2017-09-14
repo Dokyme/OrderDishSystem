1855b1f92a5cbb5ac866b0489a8e3c2e8c2de30f
Ludviq <15203500888@163.com>
2017-09-14 16:41:24 +0800

    before checkout

--- OrderDishSystemServer/src/main/java/com/odss/seu/vo/Dish.java
+++ OrderDishSystemServer/src/main/java/com/odss/seu/vo/Dish.java
@@ -43,7 +43,7 @@ public class Dish implements Serializable {
      *
      * @mbg.generated Fri Sep 08 14:07:18 CST 2017
      */
-    @JsonView(ViewLevel.SummaryWithDetail.class)
+    @JsonView(ViewLevel.Summary.class)
     private String picture;
 
     /**
